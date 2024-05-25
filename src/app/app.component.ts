b6df9f914b89d0628f0a36e1137cd5a91fcbf2f6
mdmsb <msb.arkon@gmail.com>
2024-05-25 16:41:02 +0530

    comingsoon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+import { CommonModule } from '@angular/common';
+import { ComingsoonComponent } from './screens/comingsoon/comingsoon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ComingsoonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-dreamadventures';
+  production = !environment.production;
+
 }
--- src/app/screens/comingsoon/comingsoon.component.ts
+++ src/app/screens/comingsoon/comingsoon.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './comingsoon.component.css'
 })
 export class ComingsoonComponent {
+  targetDate: Date = new Date('2024-06-03T00:00:00');
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
 
+  ngOnInit(): void {
+    this.updateCountdown();
+    setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  }
+
+  updateCountdown(): void {
+    const currentDate = new Date().getTime();
+    const distance = this.targetDate.getTime() - currentDate;
+
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  }
 }
